169d600b056b5c07358c91c002a21c62cffe7ead
Byron <byron.mena.7@gmail.com>
2022-03-06 09:48:38 -0500

    NavBar y Carousel
    Se coloca una barra de navegación y slider

---
+++ resources/views/frontend/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.front')
+
+@section('title')
+    Bienvenido a la tienda en linea
+@endsection
+
+@section('content')
+    @include('layouts.inc.slider')
+    <h1>Bienvenido</h1>
+@endsection
---
+++ resources/views/layouts/front.blade.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>
+        @yield('title')
+    </title>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <!--     Fonts and icons     -->
+    <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
+    <link href="{{asset('frontend/css/nucleo-svg.css')}}" rel="stylesheet" />
+
+    <!-- Styles -->
+    <link href="{{asset('frontend/css/argon-dashboard.css')}}" rel="stylesheet">
+    <link href="{{asset('frontend/css/custom.css')}}" rel="stylesheet">
+
+</head>
+<body>
+    @include('layouts.inc.frontnavbar')
+    <div class="min-height-300 bg-primary position-absolute w-100">
+        @yield('content')
+    </div>
+    <!-- Scripts -->
+    <script src="{{asset('frontend/js/core/bootstrap.min.js')}}"></script>
+    @if(session('status'))
+        <script>
+            swal('{{session('status')}}')
+        </script>
+    @endif
+    @yield('scripts')
+</body>
+</html>
